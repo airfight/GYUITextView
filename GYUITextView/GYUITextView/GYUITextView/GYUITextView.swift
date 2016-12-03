cd8907d6a8596f3ce04e35bde7b31b23874d9a7a
airfight <745756619@qq.com>
2016-12-03 12:05:15 +0800

    UITextView

---
+++ GYUITextView/GYUITextView/GYUITextView/GYUITextView.swift
@@ -0,0 +1,28 @@
+//
+//  GYUITextView.swift
+//  GYUITextView
+//
+//  Created by ZGY on 16/12/3.
+//  Copyright © 2016年 giant. All rights reserved.
+//
+//  Author:        Airfight
+//  My GitHub:     https://github.com/airfight
+//  My Blog:       http://airfight.github.io/
+//  My Jane book:  http://www.jianshu.com/users/17d6a01e3361
+//  Current Time:  16/12/3  下午12:01
+//  GiantForJade:  Efforts to do my best
+//  Real developers ship.
+
+import UIKit
+
+class GYUITextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
