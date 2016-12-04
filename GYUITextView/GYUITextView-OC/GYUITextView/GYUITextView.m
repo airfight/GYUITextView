ff29a0883f078b5d3a9a1117a265e1cfeffa9ddb
airfight <745756619@qq.com>
2016-12-04 21:51:11 +0800

    init

--- GYUITextView/GYUITextView-OC/GYUITextView/GYUITextView.m
+++ GYUITextView/GYUITextView-OC/GYUITextView/GYUITextView.m
@@ -17,6 +17,14 @@
 
 @implementation GYUITextView
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
